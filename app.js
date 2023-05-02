8fc177d0297ca169fd06fec3824ea4f46d345497
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2023-05-02 10:49:23 +0800

    poshy posh

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ mongoose.connect(process.env.ATLAS_URI, {
 });
 
 const corsConfig = {
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: ["https://www.dropballcity.games/", "https://www.dropballcity.games",
+    "https://dropballcity.games", "https://dropballcity.games/"],
   methods: ["GET", "POST", "PUT", "DELETE"], // List only` available methods
   credentials: true, // Must be set to true
   allowedHeaders: [
