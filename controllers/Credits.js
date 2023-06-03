e603f0c0d550bd11f8d11613262e7061dea2543f
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-06-03 13:36:44 +0800

    g
    g

--- controllers/Credits.js
+++ controllers/Credits.js
@@ -7,12 +7,11 @@ exports.send = async (req, res) => {
     const session = await Credit.startSession();
     try { 
       session.startTransaction();                  
-      const users = await Users.find({ username: [ agentusername, username ] })
-      const agentDetails = users.filter((i) => i.username == agentusername)
-      .populate({
+      const users = await Users.find({ username: [ agentusername, username ] }).populate({
         path: "roleId",
         select: "name",
-      })    
+      })
+      const agentDetails = users.filter((i) => i.username == agentusername)    
       const userDetails = users.filter((i) => i.username == username)  
       const agentWallet = await Wallets.find({ "userId": agentDetails[0]?._id })
   
