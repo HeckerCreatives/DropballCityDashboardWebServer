f3f9d2ce65f95f08127579a14fbd9bc47081ab93
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-06-14 11:14:59 +0800

    posh
    posh

--- socket.js
+++ socket.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000","https://testdbcserver.onrender.com/","https://https://testdbcserver.onrender.com","https://testdbcdashboard.onrender.com/","https://testdbcdashboard.onrender.com",],
+    // origin: ["http://localhost:3000","https://testdbcserver.onrender.com/","https://https://testdbcserver.onrender.com","https://testdbcdashboard.onrender.com/","https://testdbcdashboard.onrender.com",],
+    origin: ["https://dashboard.dropballcity.games", "https://dashboard.dropballcity.games/", "https://dropballcity.games/", "https://dropballcity.games"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: [
