051aab53a9627cb6742e8ef25df81c76b5fb51b4
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-09-04 11:32:56 +0800

    we

--- controllers/Wallets.js
+++ controllers/Wallets.js
@@ -367,10 +367,12 @@ exports.everything = async (req, res) => {
 // entity/
 exports.referrals = async (req, res) => {
   try {
-    const userList = await Users.find({deletedAt: {$exists: false}}).byRefferal(req.params.userId).populate({
+    const userList = await Users.find().byRefferal(req.params.userId).populate({
       path: "roleId",
       select: "display_name name"
-    });
+    }).then(user => {
+      return user.filter(e => !e.deletedAt)
+    })
     
     const users = userList;
 
